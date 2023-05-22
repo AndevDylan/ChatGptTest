3ceec9f7eeee3832afd7059d8db1204e82e44511
Dylan <andev.hoo@gmail.com>
2023-05-22 14:40:15 +0800

    打印kotlin测试日志与Java测试日志

---
+++ app/src/main/java/com/dylan/chatgpttest/JavaTest.java
@@ -0,0 +1,12 @@
+package com.dylan.chatgpttest;
+
+/**
+ * Creator: Dylan.
+ * Date: 2023/5/22.
+ * desc:
+ */
+public class JavaTest {
+    public void javaTest(){
+        System.out.println("JavaTest:print log");
+    }
+}
