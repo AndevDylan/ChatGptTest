b2b2f07706108f7d5fa1143f06cfc902e5e600ba
Dylan <andev.hoo@gmail.com>
2023-05-22 15:11:04 +0800

    启用databinding

--- app/src/main/java/com/dylan/chatgpttest/MainActivity.kt
+++ app/src/main/java/com/dylan/chatgpttest/MainActivity.kt
@@ -2,12 +2,18 @@ package com.dylan.chatgpttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.dylan.chatgpttest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        KotlinTest().kotlinTest()
-        JavaTest().javaTest()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.textViewTv.setOnClickListener {
+            KotlinTest().kotlinTest()
+            JavaTest().javaTest()
+        }
     }
 }
